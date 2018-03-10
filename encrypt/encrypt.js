b8858a28b8a5fdc4f15363de29bf11f99193fe67
floalex <flo.liang@my.com>
2018-03-09 16:27:52 -0800

    Add simple encrypt/decrypt for files

---
+++ encrypt/encrypt.js
@@ -0,0 +1,45 @@
+// Make a private key an environment variable
+// Perform above tasks with encrypted files
+
+// `node encrypt.js` to create password and encrypt the file
+// `node decrypt.js` to decrypt the file
+
+// crypto is the library in node.js we deal with encryption
+const crypto = require('crypto');
+// aes-256-ctr is the one recommended by the node.js community, pass in the function `crypto.createCipher`
+const algorithm = 'aes-256-ctr';  // TODO: need to pass iv and use Cipheriv later
+
+const fs = require('fs');
+// zip the large file
+const zlib = require('zlib');
+
+const filepath = 'orgexp.txt';
+// Path to temporarily store encrypted version of file to be uploaded
+const tmppath = './' + filepath + '.crypt';
+
+// create a password for the encrypt file
+const password = crypto.randomBytes(64).toString('hex');
+console.log(
+  `Please save: ${password.length} bytes of random password: ${password}`);
+
+// save the password to a secret file
+const secretpath = 'secret.env';
+// write the password in the secret file
+fs.writeFile(secretpath, password, (err) => {
+  if (err) throw err;
+  console.log('The secret file has been saved!');
+});
+
+// input file, turn it into a new ReadStream object then we can use readable.pipe
+const r = fs.createReadStream(filepath);
+// zip content
+const zip = zlib.createGzip();
+// encrypt content
+const encrypt = crypto.createCipher(algorithm, password);
+
+// write encrypted file
+const w = fs.createWriteStream(tmppath);
+
+// start pipe, stream to write encrypted
+console.log("The file is fully encrypted");
+r.pipe(zip).pipe(encrypt).pipe(w);

d3bb1d25175af22873a0b6b3bc8aa92e8f72e066
floalex <flo.liang@my.com>
2018-03-12 16:01:01 -0700

    Add mamifest file

--- upload-file/uploadfile.js
+++ upload-file/uploadfile.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const crypto = require('crypto');
+const path = require('path');
 
 function sha1Hash(file) {
   // doesn't work with `readFile`, get `undefined` for fileData
@@ -11,7 +12,31 @@ function sha1Hash(file) {
   return crypto.createHash('sha1').update(fileData).digest('hex');
 }
 
+function generateManifest(filename, filesize) {
+  return { fileName: filename, fileSize: filesize, chunks: [] };
+}
+
+function addManifestToFile(file, hashId) {
+  const sizeInByte = fs.statSync(file).size;
+  const filename = path.basename(file);
+  const manifest = generateManifest(filename, sizeInByte);
+  
+  const manifestName = hashId + '.bat';
+  const dir = './manifest';
+  
+  if (!fs.existsSync(dir)){
+    fs.mkdirSync(dir);
+  }
+  
+  return fs.writeFile(`${dir}/${manifestName}`, JSON.stringify(manifest), (err) => {
+    if (err) throw err;
+    console.log('The manifest file has been saved!');
+  });
+}
+
 const hash = sha1Hash('../stored/example.txt');
 
 console.log(hash);
+
+addManifestToFile('../stored/example.txt', hash);
 

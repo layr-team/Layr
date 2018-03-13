33653f05e1d82ca596bc8a6e456b5b8468089fc4
floalex <flo.liang@my.com>
2018-03-13 07:11:59 -0700

    Combine encrypt section to upload files

--- encrypt/decrypt.js
+++ encrypt/decrypt.js
@@ -1,28 +1,34 @@
 const crypto = require('crypto');
-const algorithm = 'aes-256-ctr';
+const algorithm = 'aes-256-cbc';
+const path = require('path');
 
 const fs = require('fs');
 const zlib = require('zlib');
 
-// get the file password from the previously designated secret file
-const secretpath = 'secret.env';
-const password = fs.readFileSync(secretpath);
-
 // file to decrpt
-const filepath = 'orgexp.txt.crypt';
-// Path to temporarily store decrypted version of file to be uploaded
-const tmppath = 'decrptexp.txt';
+const file = 'orgexp.txt.crypt';
 
-// input file
-const r = fs.createReadStream(filepath);
+const DecryptHelper = (function(filepath, mode) {
+  // Path to temporarily store decrypted version of file to be uploaded
+  const temppath = 'decrypt' + path.parse(filepath).name  // go back to original ext
 
-// decrypt content
-const decrypt = crypto.createDecipher(algorithm, password);
-// unzip content
-const unzip = zlib.createGunzip();
-// write file
-const w = fs.createWriteStream(tmppath);
+  // get the file password from the previously designated secret file
+  const secretpath = filepath + 'secret.env';
+  const password = fs.readFileSync(secretpath);
 
-// start pipe
-console.log("The file is fully decrypted");
-r.pipe(decrypt).pipe(unzip).pipe(w);
+  // input file
+  const r = fs.createReadStream(filepath);
+
+  // decrypt content
+  const decrypt = crypto.createDecipher(algorithm, password);
+  // unzip content
+  const unzip = zlib.createGunzip();
+  // write file
+  const w = fs.createWriteStream(temppath);
+
+  // start pipe
+  console.log("The file is fully decrypted");
+  r.pipe(decrypt).pipe(unzip).pipe(w);
+});
+
+DecryptHelper(file, algorithm); 

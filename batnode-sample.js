331e3efa0074d5f6331de1e78b72b1bca1813a8b
WilfredTA <rockinghamtannr@gmail.com>
2018-03-13 11:57:41 -0400

    Updating sample to demonstrate large file transfer

--- batnode-sample.js
+++ batnode-sample.js
@@ -2,39 +2,54 @@ const BatNode = require('batnode').BatNode;
 const PERSONAL_DIR = require('./utils/file').PERSONAL_DIR;
 const HOSTED_DIR = require('./utils/file').HOSTED_DIR;
 
+
 // Define callback for server to execute when a new connection has been made.
 // The connection object can have callbacks defined on it
 // Below is a node server that can respond to file retrieval requests or file storage requests
+
+// When sending image data as part of JSON object, two JSON objects are sent, each sending an incomplete JSON object
+// with only part of the image data
 const node1ConnectionCallback = (serverConnection) => {
   serverConnection.on('data', (receivedData, error) => {
+   // console.log("received data: ", receivedData)
     receivedData = JSON.parse(receivedData)
+  
+    //console.log(receivedData, "FROM SERVER")
+
     if (receivedData.messageType === "RETRIEVE_FILE") {
-      let file = node1.readFile(`./stored/${receivedData.fileName}`, (error, data) => {
-       returnData = {
-         data,
-         fileName: receivedData.fileName
+      node1.readFile(`./hosted/${receivedData.fileName}`, (error, data) => {
+       content = {
+         data
        }
-       serverConnection.write(JSON.stringify(returnData))
+       serverConnection.write(content) // sends as two chunks because JSON object too large; so sends an incomplete JSON object in buffer form. Replace "content" w/ "data" for single chunk
       })
     } else if (receivedData.messageType === "STORE_FILE"){
-      node1.writeFile(`./stored/${receivedData.fileName}-1`, JSON.stringify(receivedData.fileContent))
+      let content = new Buffer(receivedData.fileContent, 'base64')
+      node1.writeFile(`./stored/${receivedData.fileName}-1`, content)
     }
   })
 }
 
-node1.createServer(1237, '127.0.0.1', node1ConnectionCallback, node1ListenCallback)
+
+
+
+const node1 = new BatNode()
+node1.createServer(1237, '127.0.0.1', node1ConnectionCallback, null)
+
 
 // -------------------------------------
 // Example of a second node retrieving a file from a node hosting the data
 
-/*
+
+
 const node2 = new BatNode()
-node2.retrieveFile('example.txt', 1237, '127.0.0.1', (data) => {
-  data = JSON.parse(data)
-  let contents = JSON.stringify(data.data)
-  node2.writeFile(`./stored/${data.fileName}-1`, contents)
+node2.retrieveFile('image.png', 1237, '127.0.0.1', (data, fileName) => {
+  console.log(JSON.stringify(data), "What client received from server")
+  data = data.toString('base64')
+  data = new Buffer(data, 'base64')
+  node2.writeFile(`./hosted/1-${fileName}`, data)
 })
-*/
+
 
 
 // ---------------------------------------
@@ -42,9 +57,8 @@ node2.retrieveFile('example.txt', 1237, '127.0.0.1', (data) => {
 
 /*
 const node2 = new BatNode()
-node2.sendFile(1237, '127.0.0.1', './stored/example.txt', 'example.txt')
+node2.sendFile(1237, '127.0.0.1', './hosted/image.png', 'image.png')
 */
-
 
 // ---------------------------------------
 // Another example of BatNode usage...

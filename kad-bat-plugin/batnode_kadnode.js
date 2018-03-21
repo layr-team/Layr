c5e5de133c1ca3903cded885d315ac01b685d9f1
Wilfred Tannr Allard <rockinghamtannr@gmail.com>
2018-03-21 09:00:37 -0400

    updating plugin

--- kad-bat-plugin/batnode_kadnode.js
+++ kad-bat-plugin/batnode_kadnode.js
@@ -4,7 +4,7 @@ const leveldown = require('leveldown');
 const encoding = require('encoding-down');
 const kad = require('@kadenceproject/kadence');
 const BatNode = require('../batnode.js').BatNode;
-const kad_bat_interface = require('./kadence_plugin').kad_bat;
+const kad_bat = require('./kadence_plugin').kad_bat;
 const howdy = require('./kadence_plugin').howdy;
 
 
@@ -18,7 +18,7 @@ const kadnode1 = new kad.KademliaNode({
 
 
 kadnode1.listen(1338)
-kadnode1.plugin(howdy)
+//kadnode1.plugin(kad_bat)
 
 const batnode1 = new BatNode(kadnode1) // create batnode
 kadnode1.batNode = batnode1 // tell kadnode who its batnode is
@@ -37,7 +37,7 @@ kadnode2 = new kad.KademliaNode({
 })
 
 kadnode2.listen(1339)
-kadnode2.plugin(howdy)
+//kadnode2.plugin(kad_bat)
 const batnode2 = new BatNode(kadnode2)
 kadnode2.batNode = batnode2
 
@@ -45,19 +45,21 @@ kadnode2.batNode = batnode2
 
 batnode2.createServer(1900, '127.0.0.1')
 
-// kadnode2 joins kadnode1's routing table
 
-kadnode2.join([kadnode1.identity.toString('hex'), kadnode1.contact], () => {
-  
-  kadnode2.getOtherBatNodeContact([kadnode1.identity.toString('hex'), kadnode1.contact], (err, res) => {console.log(err,res)})
-  //kadnode2.ping([kadnode1.identity.toString('hex'), kadnode1.contact], (err, res) => { console.log(err, res)})
-})
+
+
 
 // kadnode2 asks kadnode1 for its batnode's contact: should log {1237, '127.0.0.1'} if successful
 
-/*kadnode2.getOtherBatNodeContact([kadnode1.identity,kadnode1.contact], (error, res)=> {
-  console.log(res, 'result')
-})*/
-//kadnode2.howdy([kadnode1.identity.toString('hex'), kadnode1.contact], (err, res) => {console.log(err,res)})
+/*kadnode2.join([kadnode1.identity, kadnode1.contact], () => {
+  kadnode2.getOtherBatNodeContact([kadnode1.identity.toString(), kadnode1.contact], (err,res) => {
+    console.log(err, res)
+  })
+})
+*/
+kadnode2.getOtherBatNodeContact([kadnode1.identity.toString(), kadnode1.contact], (err,res) => {
+  console.log(err, res)
+})
 
+//kadnode2.getOtherBatNodeContact([kadnode1.identity.toString(), kadnode1.contact], (err, res) => {console.log(err,res)})
 //kadnode2.ping([kadnode1.identity, kadnode1.contact], () => { console.log('from callback')})

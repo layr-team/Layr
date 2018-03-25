dda921a5d86ff9bef9100f11743e593975908ffe
dylankb <dylan.barnard@gmail.com>
2018-03-24 22:09:55 -0700

    Move kadence_plugin file to /

---
+++ kadence_plugin.js
@@ -0,0 +1,12 @@
+module.exports.kad_bat = function(node) {
+  node.use('BATNODE', (req, res, next) => {
+    let contact = node.batNode.address
+    res.send([contact]);
+  });
+
+  node.getOtherBatNodeContact = function(targetNode, callback) {
+    console.log('sending')
+    let batcontact = node.batNode.address
+    node.send('BATNODE', batcontact, targetNode, callback); // batcontact is an object, targetNode is a contact tuple from a bucket
+  };
+};

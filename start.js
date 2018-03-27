6aaf3b76ee5eecd0d0344852d64cae4bbe2656ef
Wilfred Tannr Allard <rockinghamtannr@gmail.com>
2018-03-27 13:10:49 -0400

    batnode.js skips nodes w/ same hostname when searching for targets and start.js starts a batnode server

--- start.js
+++ start.js
@@ -20,14 +20,43 @@ publicIp.v4().then(ip => {
   kadnode3.listen(80)
   const batnode3 = new BatNode(kadnode3)
   kadnode3.batNode = batnode3
-  batnode3.createServer(1900, ip)
+
+  const nodeConnectionCallback = (serverConnection) => {
+    serverConnection.on('end', () => {
+      console.log('end')
+    })
+    serverConnection.on('data', (receivedData, error) => {
+     receivedData = JSON.parse(receivedData)
+     console.log("received data: ", receivedData)
+  
+      if (receivedData.messageType === "RETRIEVE_FILE") {
+        batnode1.readFile(`./hosted/${receivedData.fileName}`, (error, data) => {
+         serverConnection.write(data)
+        })
+      } else if (receivedData.messageType === "STORE_FILE"){
+        let fileName = receivedData.fileName
+        batnode1.kadenceNode.iterativeStore(fileName, [batnode1.kadenceNode.identity.toString(), batnode1.kadenceNode.contact], (err, stored) => {
+          console.log('nodes who stored this value: ', stored)
+          let fileContent = new Buffer(receivedData.fileContent)
+          batnode1.writeFile(`./hosted/${fileName}`, fileContent, (err) => {
+            if (err) {
+              throw err;
+            }
+            serverConnection.write(JSON.stringify({messageType: "SUCCESS"}))
+          })
+        })
+      }
+    })
+  }
+
+  batnode3.createServer(1900, ip, nodeConnectionCallback)
   
   // Join
   
   
   kadnode3.join(seed, () => {
     console.log('you have joined the network! Ready to accept commands from the CLI!')
-    batnode3.uploadFile('./personal/example.txt')
+    //batnode3.uploadFile('./personal/example.txt')
     //batnode3.retrieveFile('./manifest/a8fe349f81906570773853d82b52a8b6bedf2a36.batchain')
   })
   

17dd72ee7c2ff6b8748f8166ae76df57b5f04a02
floalex <flo.liang@my.com>
2018-03-27 07:57:36 -0700

    Add CLIserver setup in global batnode.js

--- batnode.js
+++ batnode.js
@@ -21,6 +21,10 @@ class BatNode {
     this.address = {port, host}
   }
 
+  createCLIServer(port, host, connectionCallback) {
+    tcpUtils.createServer(port, host, connectionCallback);
+  }
+
   get server(){
     return this._server
   }

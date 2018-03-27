9344f6447ba92b562fdd510b9030c343dfb0dacc
Wilfred Tannr Allard <rockinghamtannr@gmail.com>
2018-03-27 09:48:14 -0400

    Configuring start.js for publicly accessible nodes to communicate

--- constants.js
+++ constants.js
@@ -2,7 +2,7 @@ const kad_utils = require('@kadenceproject/kadence/lib/utils');
 
 
 // For network testing:
-//exports.SEED_NODE = ['a678ed17938527be1383388004dbf84246505dbd', { hostname: '167.99.2.1', port: 80 }]
+exports.SEED_NODE = ['a678ed17938527be1383388004dbf84246505dbd', { hostname: '167.99.2.1', port: 80 }]
 
 // For local testing
 //exports.SEED_NODE = ['a678ed17938527be1383388004dbf84246505dbd', { hostname: 'localhost', port: 1338 }]
--- start.js
+++ start.js
@@ -6,12 +6,39 @@ const kad = require('@kadenceproject/kadence');
 const BatNode = require('./batnode.js').BatNode;
 const kad_bat = require('./kadence_plugin').kad_bat;
 const seed = require('./constants').SEED_NODE
+const publicIp = require('public-ip');
+
+publicIp.v4().then(ip => {
+  kadnode3 = new kad.KademliaNode({
+    transport: new kad.HTTPTransport(),
+    storage: levelup(encoding(leveldown('./dbbb'))),
+    contact: {hostname: ip, port: 80}
+  })
+  
+  // Set up
+  kadnode3.plugin(kad_bat)
+  kadnode3.listen(80)
+  const batnode3 = new BatNode(kadnode3)
+  kadnode3.batNode = batnode3
+  batnode3.createServer(1900, ip)
+  
+  // Join
+  
+  
+  kadnode3.join(seed, () => {
+    console.log('you have joined the network! Ready to accept commands from the CLI!')
+    batnode3.uploadFile('./personal/example.txt')
+    //batnode3.retrieveFile('./manifest/a8fe349f81906570773853d82b52a8b6bedf2a36.batchain')
+  })
+  
+
+})
 
 
 // Paradigm for publicly accessible nodes
 
 // capture public ip (which is the same as private ip if not behind NAT)
-// start kad node and pass it public-ip, port 8080
+// start kad node and pass it public-ip, port 80
 // start batnode server on public-ip, port 1900
 // start cli server on localhost, port 1800
 // kad node updates its contact info by connecting to tunneling server

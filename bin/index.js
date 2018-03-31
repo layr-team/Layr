07746034f3c84eca89a8f3e54d5bc9f44311852b
dylankb <dylan.barnard@gmail.com>
2018-03-31 13:44:54 -0700

    Change CLI logs, add sha command for testing

--- bin/index.js
+++ bin/index.js
@@ -19,23 +19,24 @@ batchain
   .option('-d, --download <manifestPath>', 'retrieve files from manifest file path')
   .option('-a, --audit <manifestPath>', 'audit files from manifest file path')
   .option('-p, --patch <manifestPath>', 'creates copies of vulnerable data shards to ensure data availability')
+  .option('-s, --sha <filePath>', 'Returns the SHA1 of the files content. Useful for debugging purposes')
   .parse(process.argv);
 
 const cliNode = new BatNode();
 let client;
 
 function sendUploadMessage() {
-  
+
   const message = {
     messageType: "CLI_UPLOAD_FILE",
     filePath: batchain.upload,
   };
-        
+
   client.write(JSON.stringify(message));
 }
 
 function sendDownloadMessage() {
-  
+
   const message = {
     messageType: "CLI_DOWNLOAD_FILE",
     filePath: batchain.download,
@@ -63,7 +64,7 @@ function sendAuditMessage(filePath, logOut=true) {
       if (logOut) {
         // oddly fails audit if resolve is higher in method body
         resolve(auditData);
-        console.log(`File name: ${manifest.fileName} | Manifest: ${filePath} | Data available: ${auditData.passed}`);
+        console.log(`File name: ${manifest.fileName} | Baseline redundancy met: ${auditData.passed}`);
       } else {
         resolve(auditData);
       }
@@ -84,22 +85,29 @@ function findRedundantSibling(auditData, failedSha) {
   return shardKeys.find(isRetrievabalShard);
 }
 
-async function sendPatchMessage(filePath) {
+async function sendPatchMessage(manifestPath) {
   try {
-    const audit = await sendAuditMessage(filePath);
+    const audit = await sendAuditMessage(manifestPath);
     if (!audit.passed) {
-      audit.failed.forEach((failedSha) => {
-        // patching goes here
-        const siblingShard = findRedundantShard(audit.data, failedSha);
-        if (siblingShard) {
-          // find closest batNode (host device) to shard here?
-          // const newShardId = fileSystem.createRedundantShardId(fileContent)
-          // get manifest; const manifest = fileSystem.loadManifest(filePath);
-          // write new shard to it; manifest.chunks[failedSha].push(newShardId)
+      // patching goes here
+      audit.failed.forEach((failedShaId) => {
+        const siblingShardId = findRedundantShard(audit.data, failedShaId);
+        if (siblingShardId) {
+          const message = {
+            messageType: "CLI_PATCH_FILE",
+            manifestPath: manifestPath,
+            failedShaId: failedShaId,
+            siblingShardId: siblingShardId,
+          };
+
+          client.write(JSON.stringify(message));
+
         } else {
-          console.log(`No redundant shards for ${failedSha}. You\'ll need to upload the source file`);
+          console.log(chalk.cyan(`No redundant shards for ${failedShaId}. You\'ll need to upload the source file to perform a patch`));
         }
       });
+    } else {
+      console.log(chalk.green('Your file has sufficient data redundancy across the network. No need to patch!'));
     }
   } catch(error) {
     console.log(error);
@@ -123,28 +131,28 @@ function displayFileList() {
 }
 
 if (batchain.list) {
-  
+
   displayFileList();
 
 } else if (batchain.upload) {
-  client = cliNode.connect(1800, 'localhost');  // TODO: change the hard-coded params 
+  client = cliNode.connect(1800, 'localhost');  // TODO: change the hard-coded params
 
   console.log(chalk.yellow('You can only upload one file at a time'));
-  
+
   if (!fs.existsSync(batchain.upload)) {
-    console.log(chalk.red('You entered an invalid file path, please try again'));   
+    console.log(chalk.red('You entered an invalid file path, please try again'));
   } else {
     console.log(chalk.yellow('Uploading file to the network'));
     sendUploadMessage();
   }
 
 } else if (batchain.download) {
-  client = cliNode.connect(1800, 'localhost');  // TODO: change the hard-coded params 
+  client = cliNode.connect(1800, 'localhost');  // TODO: change the hard-coded params
 
   console.log(chalk.yellow('You can only download one file at a time'));
-  
+
   if (!fs.existsSync(batchain.download)) {
-    console.log(chalk.red('You entered an invalid manifest path, please try again'));   
+    console.log(chalk.red('You entered an invalid manifest path, please try again'));
   } else {
     console.log(chalk.yellow('Downloading file to your local disk'));
     sendDownloadMessage();
@@ -152,13 +160,13 @@ if (batchain.list) {
 
 } else if (batchain.audit) {
   client = cliNode.connect(1800, 'localhost');
-  
-  console.log(chalk.yellow('You can audit file to make sure file integrity'));
-  
+
+  console.log(chalk.yellow('Auditing checks if your file is availabile on the network'));
+
   if (!fs.existsSync(batchain.audit)) {
-    console.log(chalk.red('You entered an invalid manifest path, please enter a valid file and try again'));   
+    console.log(chalk.red('You entered an invalid manifest path, please enter a valid file and try again'));
   } else {
-    console.log(chalk.yellow('sample node3 audits files from sample node1/node2'));
+    console.log(chalk.yellow('Starting file audit'));
     sendAuditMessage(batchain.audit);
   }
 } else if (batchain.patch) {

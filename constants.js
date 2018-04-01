08640bdca8a794229470ce29a871b3364c539cae
floalex <flo.liang@my.com>
2018-03-31 21:56:55 -0700

    Change to network test seed node back

--- constants.js
+++ constants.js
@@ -1,9 +1,9 @@
 // For network testing:
-// exports.SEED_NODE = ['a678ed17938527be1383388004dbf84246505dbd', { hostname: '167.99.2.1', port: 80 }];
+exports.SEED_NODE = ['a678ed17938527be1383388004dbf84246505dbd', { hostname: '167.99.2.1', port: 80 }];
 exports.CLI_SERVER = {host: 'localhost', port: 1800};
 exports.BATNODE_SERVER_PORT = 1900;
 exports.KADNODE_PORT = 80;
 
 // For local testing
-exports.SEED_NODE = ['a678ed17938527be1383388004dbf84246505dbd', { hostname: 'localhost', port: 1338 }]
+// exports.SEED_NODE = ['a678ed17938527be1383388004dbf84246505dbd', { hostname: 'localhost', port: 1338 }]
 exports.BASELINE_REDUNDANCY = 3;
